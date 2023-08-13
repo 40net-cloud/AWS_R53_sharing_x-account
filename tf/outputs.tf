899f1f307f94142840817ba2f0782f8656aceddf
xxradar <Philippe.bogaerts@radarhack.com>
2023-08-13 19:37:58 +0200

    added output

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -8,3 +8,7 @@ output "private_key" {
 output "jumbox_public_ip" {
    value = "${aws_instance.jumpbox.public_ip}"
 }
+
+output "route53_resolver_rule_share_arn" {
+   value = "${aws_route53_resolver_rule.radarhacker.arn}"
+}
