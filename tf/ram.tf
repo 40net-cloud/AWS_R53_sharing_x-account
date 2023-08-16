03bd760d6839af97971b9a8bfebc6e3c235b2b46
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2023-08-16 18:29:22 +0200

    Update ram.tf

--- tf/ram.tf
+++ tf/ram.tf
@@ -6,8 +6,8 @@ depends_on = [aws_route53_resolver_rule.radarhacker]
       provisioner "local-exec" {
         command = <<-EOT
             aws ram create-resource-share \
-            --region eu-west-3 \
-            --name MyRuleShare2 \
+            --region ${var.region} \
+            --name MyRuleShareFortigateCNFEMEA \
             --permission-arns arn:aws:ram::aws:permission/AWSRAMDefaultPermissionLicenseConfiguration \
             --resource-arns ${aws_route53_resolver_rule.radarhacker.arn} \
             --principals ${var.account-id-share}
